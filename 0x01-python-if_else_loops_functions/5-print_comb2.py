af9f59639bfa88d7aa81963b7c25bc2886efa846
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-10-03 12:46:53 -0700

    fixing task 5 last try

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for number in range(0, 100):
-    print('{:02d}'.format(number), end='')
-    if number < 99:
-        print('{:s}'.format(', '), end='')
-print()
+for number in range(0, 99):
+    print('{:02d}'.format(number), end=', ')
+print('99')
