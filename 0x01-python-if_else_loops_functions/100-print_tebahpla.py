faf518b54eef07bfa496328e5bb844bc78a793d8
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-10-03 19:07:44 -0700

    Added task 14

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for c in range(122, 96, -1):
+    if (c % 2) == 0:
+        print('{}'.format(chr(c)), end='')
+    else:
+        c -= 32
+        print('{}'.format(chr(c)), end='')
