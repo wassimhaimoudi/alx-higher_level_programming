a6f299afa5dc8cc337f5c17dbba1656365697117
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-10-05 15:10:09 -0700

    Adding task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 import sys
-
-length = len(sys.argv)
-if length == 1:
-    print('{:d} arguments.'.format((length - 1)))
-if length == 2:
-    print('{:d} argument:'.format((length - 1)))
-    print('{:d}: {:s}'.format(length - 1, sys.argv[length - 1]))
-if length > 2:
-    print('{:d} arguments:'.format((length - 1)))
-    for i in range(length, 1, -1):
-        print('{:d}: {:s}'.format((length - i + 1), sys.argv[length - i + 1]))
+if __name__ == '__main__':
+    length = len(sys.argv)
+    if length == 1:
+        print('{:d} arguments.'.format((length - 1)))
+    if length == 2:
+        print('{:d} argument:'.format((length - 1)))
+        print('{:d}: {:s}'.format(length - 1, sys.argv[length - 1]))
+    if length > 2:
+        print('{:d} arguments:'.format((length - 1)))
+        for i in range(length, 1, -1):
+            print('{:d}: {:s}'.format((length - i + 1), sys.argv[length - i + 1]))
