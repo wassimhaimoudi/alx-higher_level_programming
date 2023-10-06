f0bb6de5f96b73c1c5851a4a08347a8ab267126a
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-10-05 17:52:21 -0700

    Adding advanced tasks

---
+++ 0x02-python-import_modules/101-easy_print.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+__builtins__.__dict__['_' + '_import' + '_'] = __import__
+__builtins__.__dict__['_']('__builtins__')[2].write("#pythoniscool")
---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,13 @@
+def magic_calculation(a, b):
+    def add(x, y):
+        return (x + y)
+
+    def sub(x, y):
+        return (x - y)
+
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return (c)
+    return (sub(a, b))
