1c1aab68b8ce1047040229c06d12fd49af9bc3db
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-10-08 08:04:07 -0700

    Adding task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,9 +1,4 @@
 #!/usr/bin/pyhton3
 def print_list_integer(my_list=[]):
     for integer in my_list:
-        print('{:d}'.format(integer))
-
-
-if __name__ == '__main__':
-    mylist = [1, 4, 0, -1]
-    print_list_integer(mylist)
+        print('{:d}'.format(integer)
