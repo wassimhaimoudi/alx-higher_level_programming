e52d53315bc68ed0a50b1250c18c7d5cec29e320
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-10-09 12:02:56 -0700

    Adding task 8

--- 0x03-python-data_structures/8-main.py
+++ 0x03-python-data_structures/8-main.py
@@ -4,4 +4,6 @@ multiple_returns = __import__('8-multiple_returns').multiple_returns
 sentence = "At school, I learnt C!"
 length, first = multiple_returns(sentence)
 print("Length: {:d} - First character: {}".format(length, first))
-
+sentence1 = ""
+length, first = multiple_returns(sentence1)
+print("Length: {:d} - First character: {}".format(length, first))
