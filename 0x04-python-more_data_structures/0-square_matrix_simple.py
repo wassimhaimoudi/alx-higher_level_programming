8d0d6bd7eaf8615151b1d5ca15645f1219eb57fc
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-10-11 15:53:46 -0700

    Adding task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    sqrt_matrix = [list(map(lambda x: x**2, row)) for row in matrix]
+    sqrt_matrix = list(map(lambda r: list(map(lambda x: x**2, r)), matrix))
     return sqrt_matrix
