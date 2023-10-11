be8d584f2e12c9256672e4244b8d2b55069ac221
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-10-11 13:49:47 -0700

    Adding task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,8 +4,5 @@ def square_matrix_simple(matrix=[]):
     A function that computes the square
     value of all integers in a matrix
     """
-    new_matrix = []
-    for row in matrix:
-        squared = list(map(lambda x : x**2, row))
-        new_matrix.append(result)
-    return (new_list)
+    sqrt_matrix = [list(map(lambda x: x**2, row)) for row in matrix]
+    return (sqrt_matrix)
