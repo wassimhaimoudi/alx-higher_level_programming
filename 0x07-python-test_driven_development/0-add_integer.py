b350ddf96245ad9e6b09fed5201dc5e30b9156bf
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-10-29 06:24:46 -0700

    Adding task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+
+
 """
 This module defines the `add_integer` function
 """
+
+
 def add_integer(a, b=98):
     """
     Adds two integers with one taking the default value
@@ -13,13 +17,11 @@ def add_integer(a, b=98):
 
     Returns:
         (int): The sum of a and b
-
     """
     if not (isinstance(a, int) or isinstance(a, float)):
         raise TypeError('a must be an integer')
-    if not (isinstance(b, int)or isinstance(b, float)):
+    if not (isinstance(b, int) or isinstance(b, float)):
         raise TypeError('b must be an integer')
     a = int(a)
     b = int(b)
     return (a + b)
-    
