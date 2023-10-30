2309cdad100f2a6cf211fec6aeb5ca8cf81b3cb6
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-10-30 15:18:14 -0700

    Adding Task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -13,6 +13,14 @@ class Rectangle:
             height (int): The height of the new rectangle.
 
         """
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
         self.__width = width
         self.__height = height
 
