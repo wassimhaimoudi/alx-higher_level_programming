75b9cac96fef9ec11c3525bfaefb50e76704ed8d
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-11-06 11:48:31 -0800

    Adding task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module contains the definition of a class"""
+
+
+class MyList(list):
+    """Represents a list"""
+    def __init__(self):
+        """
+        Instantiation of the MyList instance
+        Invokation of the parent class
+        """
+        super().__init__()
+
+    def print_sorted(self):
+        print(str(self))
