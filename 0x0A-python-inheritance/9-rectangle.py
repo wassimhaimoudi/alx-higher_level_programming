1920ccd81dcb4e419d615ef8159d320688b25af7
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-11-06 18:00:51 -0800

    Adding Task 9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -39,5 +39,4 @@ class Rectangle(BaseGeometry):
          Returns:
             str: a string showing the rectangle dimensions
         """
-        return ("[{}] {:d}/{:d}\
-                ".format(Rectangle.__name__, self.__width, self.__height))
+        return ("[Rectangle] {:d}/{:d}".format(self.__width, self.__height))
