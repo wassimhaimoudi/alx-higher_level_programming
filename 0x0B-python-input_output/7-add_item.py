1b6d4e3a467b886f97b90528131b02eb91abdcc3
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-11-10 11:23:42 -0800

    Adding Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
-"""This module contains a script to add itmes to a list"""
+"""Add all arguments to a Python list and save them to a file."""
+import sys
 
-if __name__ == '__main__':
-    import sys
-
-    load = __import__('6-load_from_ json_file').load_from_json_file
+if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-    filename = 'add_item.json'
     try:
-        my_list = load(filename)
+        items = load_from_json_file("add_item.json")
     except FileNotFoundError:
-        my_list = []
-    for i in range(1, len(sys.argv)):
-        my_list.append(sys.argv[i])
-
-    save_to_json_file(my_list, filename)
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
