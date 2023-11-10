81a4d78bcc3c0a4bf0b4d514763ebcfc16873a40
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-11-10 11:15:12 -0800

    Adding Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 """This module contains a script to add itmes to a list"""
-import sys
 
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+if __name__ == '__main__':
+    import sys
 
+    load_from_json_file = __import__('6-load_from_\
+            json_file').load_from_json_file
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-my_list = load_from_json_file('add_item.json')
-for i in range(1, len(sys.argv)):
-    my_list.append(sys.argv[i])
+    filename = 'add_item.json'
+    try:
+        my_list = load_from_json_file(filename)
+    except FileNotFoundError:
+        my_list = []
+    for i in range(1, len(sys.argv)):
+        my_list.append(sys.argv[i])
 
-save_to_json_file(my_list, 'add_item.json')
+    save_to_json_file(my_list, filename)
