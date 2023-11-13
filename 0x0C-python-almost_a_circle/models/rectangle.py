daed42bd2e9b0eea96c5062e7504961e8f6c7040
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-11-13 08:31:03 -0800

    Adding task 2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -18,22 +18,6 @@ class Rectangle(Base):
             y(int): The position of the rectangle on the y axis
 
         """
-        if not isinstance(width, int):
-            raise TypeError('width must be an integer')
-        if not isinstance(height, int):
-            raise TypeError('height must be an integer')
-        if not isinstance(x, int):
-            raise TypeError('x must be an integer')
-        if not isinstance(y, int):
-            raise TypeError('y must be an integer')
-        if width <= 0:
-            raise ValueError('width must be > 0')
-        if height <= 0:
-            raise ValueError('height must be > 0')
-        if x < 0:
-            raise ValueError('x must be >= 0')
-        if y < 0:
-            raise ValueError('y must be >= 0')
         self.__width = width
         self.__height = height
         self.__x = x
