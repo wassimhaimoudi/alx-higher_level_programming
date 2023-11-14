6f982cf31b1423aa3f855f8bad0a3fcde230bc7d
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-11-14 15:51:10 -0800

    Added Task 17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -37,6 +37,21 @@ class Base:
             return '[]'
         return json.dumps(list_dictionaries)
 
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        Returns the list of the JSON string representation
+
+        Args:
+            json_string(str): JSON string representation
+
+        Returns:
+            list: A list of the JSON string representation
+        """
+        if json_string is None or json_string == '':
+            return '[]'
+        return json.loads(json_string)
+
     @classmethod
     def save_to_file(cls, list_objs):
         """
