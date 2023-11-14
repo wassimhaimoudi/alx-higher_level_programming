629d714a766fcca10d96cbc9ced33048aa6954b6
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-11-13 18:21:17 -0800

    Adding task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,6 +23,6 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        if list_dictionaries is None or (not list_dictionaries):
+        if list_dictionaries is None or list_dictionaries == []:
             return []
         return json.dumps(list_dictionaries)
