a531e633e3e4f6fe8eecb25e9bb68f592ae8374e
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-11-13 18:19:27 -0800

    Adding task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This module defines a base class called `Base`"""
+import json
 
 
 class Base:
@@ -19,3 +20,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or (not list_dictionaries):
+            return []
+        return json.dumps(list_dictionaries)
