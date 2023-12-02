fd8dd86be3108046e915597cff9aa70c7639121c
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-12-02 05:09:03 -0800

    Added Task 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+"""This module defines the `Student` class"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Instantiation of the Student object
+
+        Args:
+            (str): The first name of the student
+            (str): The last name of the student
+            (int): The age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of the class Student
+
+        Returns:
+            dict: A dictionary representation of a Student class
+                instance
+        """
+        return {
+                'first_name': self.first_name,
+                'last_name': self.last_name,
+                'age': self.age
+                }
