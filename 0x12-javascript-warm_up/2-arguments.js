84ae24246c6556ac23a4c53b61aef7001aa97bbd
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2024-01-08 11:20:30 -0800

    Added Task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,6 +2,8 @@
 // console.log(process.argv);
 if (process.argv.length === 2) {
   console.log('No argument');
-} else {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
