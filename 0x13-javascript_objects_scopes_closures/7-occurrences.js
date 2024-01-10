20d56e78f3c62e5cb5dce9a124b30baf665f8c05
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2024-01-10 14:34:47 -0800

    Added Task 7

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let i =0;
+  let counter = 0;
+  while (i < list.length) {
+    if (list[i] === searchElement) {
+      counter++;
+    }
+    i++;
+  }
+  return counter;
+}
