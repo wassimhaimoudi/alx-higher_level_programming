365a7652bad09273ce2983005f905ee96010f8fc
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2024-01-10 15:22:08 -0800

    Added task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  return list.reverse();
+  for (let i = 0; i < list.length / 2; i++) {
+    const temp = list[i];
+    list[i] = list[list.length - i - 1];
+    list[list.length - i - 1] = temp;
+  }
+  return list;
 };
