a604485e042ed703c988cfa5155a1adc3efb4fa8
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2024-01-10 16:42:46 -0800

    Added Task 11

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
 
-const mapped = list.map((x) => x * list.indexOf(x));
+const mapped = list.map((x, index, list) => x * index);
 console.log(list);
 console.log(mapped);
