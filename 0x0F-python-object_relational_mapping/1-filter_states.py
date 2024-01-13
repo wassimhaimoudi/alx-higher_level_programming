848f94eab1f2f5aaef8d0d453a940e8a993a9783
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2024-01-13 12:24:56 -0800

    Added Task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" This module lists all states from the database hbtn_0e_0_usa """
+""" This module lists all states where name starts with 'N'
+from the database hbtn_0e_0_usa """
 if __name__ == '__main__':
     import sys
     import MySQLdb
