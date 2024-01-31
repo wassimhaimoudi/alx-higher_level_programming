fcc258a9c96e62b1acdf6b03c5f3f6bf8551c6bb
Wassim Haimoudi <105460999+wassimhaimoudi@users.noreply.github.com>
2024-01-31 06:30:31 +0100

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-
-exports.addMeMaybe = function addMeMaybe (number, thisFunction) {
-  number++;
-  thisFunction(number);
-};
--- 0x12-javascript-warm_up/4-concat.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/node
-
-const myString = process.argv[2] + ' is ' + process.argv[3];
-console.log(myString);
