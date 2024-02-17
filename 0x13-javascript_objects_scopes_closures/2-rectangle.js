62073320f785b25aa2b82849ed68b7277dc88e47
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2024-02-17 06:16:24 -0800

    Fixed Task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-
-module.exports = class Rectangle {
-  contructor (w, h) {
-    if (w > 0 && h > 0) {
+class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
       this.width = w;
-      this.height = h;	    
+      this.height = h;
     }
   }
-};
+}
+module.exports = Rectangle;
