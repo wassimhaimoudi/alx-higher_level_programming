4e2fcb06764204124c7b7cd67f63c130050dc27d
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2024-02-23 07:47:13 -0800

    Added Task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
             db=my_db
             )
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE states.name = '{}'\
+    query = "SELECT * FROM states WHERE states.name LIKE BINARY '{}'\
             ORDER BY states.id".format(s_name)
 
     cur.execute(query)
