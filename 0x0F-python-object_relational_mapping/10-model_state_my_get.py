7507a610762ddbc5aa7719b2059ae282f8e0c816
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2024-02-29 07:13:00 -0800

    Added Task 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""This script lists all State objects from the database hbtn_0e_6_usa
+"""
+if __name__ == "__main__":
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from sys import argv
+
+    my_usr = argv[1]
+    my_pwd = argv[2]
+    my_db = argv[3]
+    my_state = argv[4]
+
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'.
+            format(my_usr, my_pwd, my_db)
+            )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result_state = session.query(State).filter(
+            State.name.like('{}'.format(my_state))
+            ).order_by(State.id).first()
+
+    if result_state:
+        print(result_state.id)
+    else:
+        print('Not found')
+
+    session.close()
