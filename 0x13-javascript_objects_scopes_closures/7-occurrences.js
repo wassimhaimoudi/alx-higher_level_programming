907a5c6fb2474b2519bffd62d8aedfe32af59c58
Wassim Haimoudi <105460999+wassimhaimoudi@users.noreply.github.com>
2024-04-01 05:02:13 +0200

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-exports.nbOccurences = function (list, searchElement) {
-  let counter = 0;
-  for (const x of list) {
-    if (x === searchElement) { counter++; }
-  }
-  return counter;
-};
