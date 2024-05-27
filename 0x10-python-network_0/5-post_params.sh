05c7eb0a54d74f4fb88a6982fee86eedf6bf72e9
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2024-05-27 06:32:11 +0100

    Fixed a bug

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a `POST` request to the specified url and returns the body of the response
-curl -s -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%PLD" "$1"
+curl -s -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
