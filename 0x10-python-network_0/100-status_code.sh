432281acb358c5847bfc8540015ed2dee47340ad
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2024-05-29 00:52:22 +0100

    Added task 7 from adv tasks

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a request to a url and displays displays the status code of the response
-curl -sI "$1" | head -n 1 | cut -d " " -f 2
+curl -so /dev/null -w "%{http_code}" "$1"
