c3c91796fa58ea882d021698c0d3b19cb892d383
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2024-05-30 00:17:27 +0100

    Fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,9 +5,12 @@ import urllib.request
 
 
 if __name__ == '__main__':
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
-        body = resp.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode('utf-8')))
+    try:
+        with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
+            body = resp.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(body)))
+            print("\t- content: {}".format(body))
+            print("\t- utf8 content: {}".format(body.decode('utf-8')))
+    except Exception as e:
+        pass
