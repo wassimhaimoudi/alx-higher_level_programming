47457a0e06d7ef10316581a516135817f71c97c1
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2024-05-29 14:55:41 +0100

    minor fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""A script that
-fetches https://intranet.hbtn.io/status.
+"""This script fetches https://intranet.hbtn.io/status.
 """
+import urllib.request
 
 
 if __name__ == '__main__':
-    import urllib.request
-
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
         body = resp.read()
         print("Body response:")
