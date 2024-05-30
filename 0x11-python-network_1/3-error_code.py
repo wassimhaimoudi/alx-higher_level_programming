3443c6ed990c395130fe7ebd28ee40e7773bd1db
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2024-05-30 01:43:25 +0100

    minor fix :3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""This module takes in a URL, sends a request to the URL,
-and displays the body of the response (decoded in utf-8).
+"""Fetches a url and handles errors
 """
-import urllib.request
-import urllib.error
-from sys import argv
-
-url = argv[1]
-try:
-    with urllib.request.urlopen(url) as resp:
-        print(resp.read().decode('utf-8'))
-except urllib.error.HTTPError as e:
-    print("Error code: {}".format(e.code))
+if __name__ == '__main__':
+    import urllib.request
+    import urllib.error
+    from sys import argv
+    url = argv[1]
+    try:
+        with urllib.request.urlopen(url) as resp:
+            print(resp.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
