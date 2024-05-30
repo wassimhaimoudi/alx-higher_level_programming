cdb68567850985fb41bef888b9b978e340937921
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2024-05-30 01:30:52 +0100

    Added solution to task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-""" This script takes in a url,
-sends a request to the url and displays
-the body ofthe response(decoded in utf-8)
+"""This script takes in a url, sends a request to the url
+and displays the body ofthe response(decoded in utf-8)
 """
 import urllib.request
 import urllib.error
@@ -9,6 +8,7 @@ from sys import argv
 
 
 url = argv[1]
+
 if __name__ == '__main__':
     try:
         with urllib.request.urlopen(url) as resp:
