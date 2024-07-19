c66a3e47f120a1129855e00dac2da418d0d76e31
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2024-07-18 22:32:15 -0700

    Typo fix

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 
-const url = "https://swapi-api.alx-tools.com/api/films/" + process.argv[2] + "/";
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2] + '/';
 
 request(url, function (err, response, body) {
   if (err) {
