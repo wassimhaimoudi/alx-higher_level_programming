9ee24525981bdf2c1f0c886112529bb982146e3b
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2024-07-18 22:42:16 -0700

    Updated 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 fs.readFile(process.argv[2], 'utf-8', function (err, data) {
   if (err) {
     console.error(err);
-	  return;
+    return;
   }
   console.log(data);
 });
